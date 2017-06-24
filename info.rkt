29319b564177669dbe93d302b7d4aa7a51115a96
AlexKnauth <alexander@knauth.org>
2017-06-24 18:42:43 -0500

    add info.rkt and .gitignore

---
+++ info.rkt
@@ -0,0 +1,15 @@
+#lang info
+
+(define collection "turn-based-game")
+
+(define deps
+  '("base"
+    "agile"
+    "collections-lib"
+    "htdp-lib"
+    ))
+
+(define build-deps
+  '("rackunit-lib"
+    ))
+
