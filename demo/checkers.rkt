d7726375a5b1f439b2f489bfb9d7421569070f61
AlexKnauth <alexander@knauth.org>
2018-08-09 12:38:53 -0400

    checkers: show how to run 2-player

--- demo/checkers.rkt
+++ demo/checkers.rkt
@@ -6,6 +6,10 @@
 
 (module+ main
 
+  ;Human vs. Human
+  ;(start/computer CHECKERS (hash))
+
+  ;Human vs. Computer
   (start/computer CHECKERS (hash O (computer/score-explore-random 2 3 40)))
 
   )
